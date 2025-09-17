e56b90fca1578ddca857c7670cbdcbeb66bf6cfa
Nathan Daneliak <nadandevelop@gmail.com>
2025-09-16 22:04:58 -0400

    Created VS Project and Begin
    Print out rgb image in ppm file format with progress tracker.

---
+++ RayTracer/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int imageWidth = 256;
+	int imageHeight = 256;
+
+	cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+	for (int j = 0; j < imageHeight; j++)
+	{
+		clog << "\rScanlines remaining: " << (imageHeight - j) << '\n';
+		for (int i = 0; i < imageWidth; i++)
+		{
+			auto r = double(i) / (imageWidth - 1);
+			auto g = double(j) / (imageHeight - 1);
+			auto b = 0.0;
+
+			int ir = int(255.999 * r);
+			int ig = int(255.999 * g);
+			int ib = int(255.999 * b);
+
+			cout << ir << ' ' << ig << ' ' << ib << "\n";
+		}
+	}
+
+	clog << "Done.\n";
+}
