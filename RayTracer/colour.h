cc2d8a17bf597c6566258fc062c11250fe713608
Nathan Daneliak <nadandevelop@gmail.com>
2025-09-18 23:15:07 -0400

    Rays and colours
    Added colour header file with the writeColour function to print out a colour. Added ray header file to track ray positions. Can now render background of the scene.

---
+++ RayTracer/colour.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+
+#include "vec3.h"
+
+using namespace std;
+
+using colour = vec3;
+
+void writeColour(ostream& out, const colour& pixelColour)
+{
+	auto r = pixelColour.x();
+	auto g = pixelColour.y();
+	auto b = pixelColour.z();
+
+	int rByte = int(255.999 * r);
+	int gByte = int(255.999 * g);
+	int bByte = int(255.999 * b);
+
+	out << rByte << ' ' << gByte << ' ' << bByte << '\n';
+}
--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
 
+#include "vec3.h"
+#include "ray.h"
+#include "colour.h"
+
 using namespace std;
+
+colour rayColour(const ray& r)
+{
+	vec3 unitDirection = unitVector(r.direction());
+	auto a = 0.5 * (unitDirection.y() + 1.0); // a must be between 0 and 1 instead of -1 to 1
+	return (1.0 - a) * colour(1.0, 1.0, 1.0) + a * colour(0.5, 0.7, 1.0); // Lerp
+}
 
 int main()
 {
-	int imageWidth = 256;
-	int imageHeight = 256;
+	auto aspectRatio = 16.0 / 9.0;
+	int imageWidth = 400;
 
+	// Image Height : Must be at least 1
+	int imageHeight = int(imageWidth / aspectRatio);
+	imageHeight = (imageHeight < 1) ? 1 : imageHeight;
+
+	// Camera
+	auto focalLength = 1.0;
+	auto viewportHeight = 2.0;
+	auto viewportWidth = viewportHeight * (double(imageWidth) / imageHeight);
+	auto cameraCenter = point3(0, 0, 0);
+
+	// Vertical and Horizontal vectors
+	auto viewportU = vec3(viewportWidth, 0, 0);
+	auto viewportV = vec3(0, -viewportHeight, 0);
+
+	// Horizontal and vertical delta vectors from pixel to pixel
+	auto pixelDeltaU = viewportU / imageWidth;
+	auto pixelDeltaV = viewportV / imageHeight;
+
+	// Upper left pixel
+	auto viewportUpperLeft = cameraCenter - vec3(0, 0, focalLength) - viewportU / 2 - viewportV / 2;
+	auto pixel00Loc = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV); // Middle
+
+
+	// Render
 	cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
 
 	for (int j = 0; j < imageHeight; j++)
@@ -14,15 +49,12 @@ int main()
 		clog << "\rScanlines remaining: " << (imageHeight - j) << '\n';
 		for (int i = 0; i < imageWidth; i++)
 		{
-			auto r = double(i) / (imageWidth - 1);
-			auto g = double(j) / (imageHeight - 1);
-			auto b = 0.0;
+			auto pixelCenter = pixel00Loc + (i * pixelDeltaU) + (j * pixelDeltaV);
+			auto rayDirection = pixelCenter - cameraCenter;
+			ray r(cameraCenter, rayDirection);
 
-			int ir = int(255.999 * r); // Avoid truncation errors
-			int ig = int(255.999 * g);
-			int ib = int(255.999 * b);
-
-			cout << ir << ' ' << ig << ' ' << ib << "\n";
+			colour pixelColour = rayColour(r);
+			writeColour(cout, pixelColour);
 		}
 	}
 
