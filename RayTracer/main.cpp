866989d47fb5959877cdf553866840b8ee9941b9
Nathan Daneliak <nadandevelop@gmail.com>
2025-09-21 21:34:40 -0400

    Added sphere hit check

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -9,8 +9,23 @@
 
 using namespace std;
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
 colour rayColour(const ray& r)
 {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+	{
+		return colour(1, 0, 0);
+	}
+
 	vec3 unitDirection = unitVector(r.direction());
 	auto a = 0.5 * (unitDirection.y() + 1.0); // a must be between 0 and 1 instead of -1 to 1
 	return (1.0 - a) * colour(1.0, 1.0, 1.0) + a * colour(0.5, 0.7, 1.0); // Lerp
